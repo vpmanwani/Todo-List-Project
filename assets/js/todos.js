e7af57e9db8f5865e7699fdd7d757d0b6135159f
Vishal Manwani <vpmanwani2204@gmail.com>
2020-03-15 22:58:12 +0530

    Add files via upload

---
+++ assets/js/todos.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+	//check of f specific todos by clicking
+	$("ul").on("click", "li", function(){
+		$(this).toggleClass("completed");
+	});	
+
+	//click on X to delete todo
+	$("ul").on("click", "span", function(event){
+		$(this).parent().fadeOut(500,function(){
+			$(this).remove();
+		});
+		event.stopPropagation();
+	});
+
+	$("input[type='text']").keypress(function(event){
+		if(event.which === 13){
+			//grab new todo text
+			var todoText = $(this).val();
+
+			//create a new li and add to ul
+			$("ul").append("<li><span><i class = 'fa fa-trash'></i> </span>"+todoText+"</li>");
+			$(this).val("");
+		}
+	})
+
+	$(".fa-plus").click(function(){
+		$("input[type='text']").fadeToggle();
+	});
+
+});
+
